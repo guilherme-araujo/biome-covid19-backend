c8f5f23026bb8ec8e100a2c9e31df2f99dcbec17
gfaraujo <guilherme.araujo@imd.ufrn.br>
2020-06-02 12:12:51 -0300

    Routes for new data

--- src/routes.js
+++ src/routes.js
@@ -36,4 +36,36 @@ routes.get('/covid_norm-plot4', function (req, res) {
     });
 });
 
+routes.get('/covid_norm-06-01', function (req, res) {    
+    fs.readFile('external/covid_norm-06-01.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        
+        res.send(data);
+    });
+});
+
+routes.get('/covid_norm-plot2-06-01', function (req, res) {    
+    fs.readFile('external/covid_norm-plot2-06-01.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        
+        res.send(data);
+    });
+});
+
+routes.get('/covid_norm-plot3-06-01', function (req, res) {    
+    fs.readFile('external/covid_norm-plot3-06-01.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        
+        res.send(data);
+    });
+});
+
+routes.get('/covid_norm-plot4-06-01', function (req, res) {    
+    fs.readFile('external/covid_norm-plot4-06-01.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        
+        res.send(data);
+    });
+});
+
 module.exports = routes;
